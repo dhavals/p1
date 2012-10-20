cd1323c3c5f1d7ee9c7fbd3e1e9fd11d0f30a8de
Dhaval Shah <dhavals@andrew.cmu.edu>
2012-10-20 04:01:26 -0400

    fair bit done but accelerometer not working on mobile

--- GameAccel.js
+++ GameAccel.js
@@ -2,8 +2,26 @@
 window.onload = function(){
 
     var accelerometer;
+    var points = 100;
+    var observedValues = {"x": 0, "y" : 0, "z": 0};
+    var targetValues = {"x": 0, "y" : 0, "z": 0};
+
+    var pointsElement = $('#points').get(0);
+
+    var observedElements = {"x": $('#xObserved').get(0), 
+                            "y": $('#yObserved').get(0), 
+                            "z": $('#zObserved').get(0)};
+
+    var targetElements =   {"x": $('#xTarget').get(0), 
+                            "y": $('#yTarget').get(0), 
+                            "z": $('#zTarget').get(0)};
+
+
+    pointsID = setInterval(decrementPoints, 1000); // every second, reduce 1 point
+    screenID = setInterval(updateScreen, 100); // update all numbers on screen every 100 ms
 
     setup();
+    initGame();
     runGame();
 
     function setup()
@@ -12,8 +30,36 @@ window.onload = function(){
         accelerometer.startListening();
     }
 
+    function initGame()
+    {
+
+    }
+
     function runGame()
     {
+    }
+
+    function decrementPoints()
+    {
+        if (points > 0) points--;
+    }
+
+    function updateScreen()
+    {
+        pointsElement.innerHTML = "Score: " + points;
         
+        targetElements.x.innerHTML = targetValues.x;
+        targetElements.y.innerHTML = targetValues.y;
+        targetElements.z.innerHTML = targetValues.z;
+
+        observedValues = accelerometer.getLast();
+
+        observedElements.x.innerHTML = observedValues.x;
+        observedElements.y.innerHTML = observedValues.y;
+        observedElements.z.innerHTML = observedValues.z;
     }
 }
+
+
+
+
